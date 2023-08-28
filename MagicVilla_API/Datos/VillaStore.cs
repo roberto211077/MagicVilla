ea2dce02e536799f10c1731d008387a5667d797c
Roberto Velazquez <[roberto211077@live.com]>
2023-08-28 19:32:42 -0300

    clase19

---
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_API.Modelos.Dto;
+
+namespace MagicVilla_API.Datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto> 
+        {
+            new VillaDto{Id=1, Nombre = "Vista a la Piscina"},
+            new VillaDto{Id=2,Nombre = "Vista a la Playa"}
+        };
+    }
+}
